5a8d20807d81d2aaeb9679efa7d517708009db9a
Brian Warner <warner@lothar.com>
2020-05-29 10:57:10 -0700

    add harmonic minor scale

--- midi-key-finder.js
+++ midi-key-finder.js
@@ -267,6 +267,7 @@ var all_modes = [
     ["Locrian", [0,1,3,5,6,8,10]],
     ["diminished", [0,1,3,4,6,7,9,10]],
     ["Hungarian minor", [0,2,3,6,7,8,11]], // Pink Panther = A
+    ["harmonic minor", [0,2,3,5,7,8,11]],
     ["minor blues", [0,3,5,6,7,10]],
     ["minor pentatonic", [0,3,5,7,10]],
     ["major pentatonic", [0,2,4,7,9]],
